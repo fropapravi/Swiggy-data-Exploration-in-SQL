a610ae5df15c16b0f22469944fdc6ac731da1025
Praveen Kumar B <157269668+fropapravi@users.noreply.github.com>
2024-05-30 22:42:21 +0530

    Update Swiggy.sql

--- Swiggy.sql
+++ Swiggy.sql
@@ -5,16 +5,24 @@ Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, 
 
 */
 
--- find the top one resturant in banglore
+--import data to Mysql -- explained in Readme file
+
+
+
+-- 1. find the top one resturant in banglore
+
 select distinct restaurant_name,city, rating,
 rank() over (order by rating desc) as 'rank'
 from swiggy
 where city = 'bangalore' and rating > 4.5
 limit 1;
--- How many restaurants have rating greater than 4.5
+
+-- 2. How many restaurants have rating greater than 4.5
+
 select count(distinct restaurant_name ) as number_of_Restaurant
 from swiggy
 where rating >4.5;
+
 -- 3.	Which is the top one city with highest number of restaurant?
 
 select count(distinct restaurant_name) as total_restaurant, city
@@ -27,8 +35,8 @@ limit 1;
 
 select distinct restaurant_name, rating,
 rank () over (order by restaurant_name desc) as title_name
- from swiggy
- where restaurant_name like '%pizza%';
+from swiggy
+where restaurant_name like '%pizza%';
  
  
  -- 5.	What is the most common cuisine among the resturants in dataset?
