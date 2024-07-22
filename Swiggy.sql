68e51b2a16db6fa1565310bfde2510cd804e4a79
Praveen Kumar B <157269668+fropapravi@users.noreply.github.com>
2024-07-22 11:54:37 +0530

    Update Swiggy.sql

--- Swiggy.sql
+++ Swiggy.sql
@@ -5,7 +5,6 @@ Basic level
 
 --import data to Mysql -- explained in Readme file
 
-
 # find the top one resturant in banglore
 
 select distinct restaurant_name,city, rating,
@@ -14,13 +13,13 @@ from swiggy
 where city = 'bangalore' and rating > 4.5
 limit 1;
 
--- 2. How many restaurants have rating greater than 4.5
+# How many restaurants have rating greater than 4.5
 
 select count(distinct restaurant_name ) as number_of_Restaurant
 from swiggy
 where rating >4.5;
 
--- 3.	Which is the top one city with highest number of restaurant?
+#	Which is the top one city with highest number of restaurant?
 
 select count(distinct restaurant_name) as total_restaurant, city
 from swiggy
@@ -28,7 +27,7 @@ group by city
 order by total_restaurant desc
 limit 1;
 
--- 4.	How many restaurants have word “pizza” in their name?
+#	How many restaurants have word “pizza” in their name?
 
 select distinct restaurant_name, rating,
 rank () over (order by restaurant_name desc) as title_name
@@ -36,7 +35,7 @@ from swiggy
 where restaurant_name like '%pizza%';
  
  
- -- 5.	What is the most common cuisine among the resturants in dataset?
+#	What is the most common cuisine among the resturants in dataset?
  
 select cuisine, count(*) as cuisine_title
 from swiggy
@@ -44,24 +43,17 @@ group by cuisine
 order by cuisine_title desc
 limit 1;
 
--- 6.	What is the average rating of restaurant in each city
+#	What is the average rating of restaurant in each city
 
 select distinct city, avg(rating) 
 from swiggy
 group by city
 order by city desc;
 
--- 7.	What is the highest price of the item under ‘recommended’ menu 
--- category for each restaurants?
+#	What is the highest price of the item under ‘recommended’ menu category for each restaurants?
 
--- select * from swiggy
+select * from swiggy
 select distinct restaurant_name, menu_category, price
 from swiggy
 where menu_category = 'recommended'
-
-
-
- 
- 
-
 
